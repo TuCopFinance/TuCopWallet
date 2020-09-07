celo-org/celo-monorepo

commit 84e90db6a8271188156dfab593a627085cc87cf0

[Wallet] Fix bug to allow users to skip invite redemption after canceling (#4977)
### Description
If a user presses "Cancel" while an invite redemption is in progress, it should reset the invite redemption state variables. Currently, `isRedemeeingInvite` is never flipped back to `false`. Additionally, when a invite redemption fails it often does so after an account has been created but before the funds have been transferred from the temp invite account to the user's account. The combination of `account` and `isRedeemingInvite` being truthy results in the user being stuck on the `EnterInviteCode` screen indefinitely.

### Other changes
- When a user copy/pastes the full invite text message, the input form used to get populated with the private key that was extracted from the parsed invite code. This was confusing users so instead we are now populating the input form with the parsed invite code instead of the private key
- Added a cancel action, which gets triggered when the user presses "cancel" on the `EnterInviteCode` screen,  to the send transaction within `moveAllFundsFromAccount` 
- Changed the condition by which `addTempAccountToWallet` detects whether an account has already been created. I think this may have gotten overlooked during the recent geth changes

### Tested
Added tests and ran through various conditions on a physical device.

### Related issues
NA

### Backwards compatibility
Yes

packages/mobile/src/invite/saga.ts
@@ -8,7 +8,18 @@ import DeviceInfo from 'react-native-device-info'
 import { asyncRandomBytes } from 'react-native-secure-randombytes'
 import SendIntentAndroid from 'react-native-send-intent'
 import SendSMS from 'react-native-sms'
-import { all, call, delay, put, race, spawn, take, takeLeading } from 'redux-saga/effects'
+import {
+  all,
+  call,
+  delay,
+  put,
+  race,
+  spawn,
+  take,
+  TakeEffect,
+  takeLeading,
+} from 'redux-saga/effects'
+import { Actions as AccountActions } from 'src/account/actions'
 import { showError, showMessage } from 'src/alert/actions'
 import { InviteEvents, OnboardingEvents } from 'src/analytics/Events'
 import ValoraAnalytics from 'src/analytics/ValoraAnalytics'
@@ -280,31 +291,37 @@ function* sendInviteSaga(action: SendInviteAction) {
   }
 }
 
-export function* redeemInviteSaga({ inviteCode }: RedeemInviteAction) {
+export function* redeemInviteSaga({ tempAccountPrivateKey }: RedeemInviteAction) {
   yield call(waitWeb3LastBlock)
   Logger.debug(TAG, 'Starting Redeem Invite')
 
   const {
     result,
+    cancel,
     timeout,
   }: {
-    result: { success: true; newAccount: string } | { success: false }
-    timeout: true
+    result: { success: true; newAccount: string } | { success: false } | undefined
+    cancel: TakeEffect | undefined
+    timeout: true | undefined
   } = yield race({
-    result: call(doRedeemInvite, inviteCode),
+    result: call(doRedeemInvite, tempAccountPrivateKey),
+    cancel: take(AccountActions.CANCEL_CREATE_OR_RESTORE_ACCOUNT),
     timeout: delay(REDEEM_INVITE_TIMEOUT),
   })
 
-  if (result.success === true) {
+  if (result?.success === true) {
     Logger.debug(TAG, 'Redeem Invite completed successfully')
     yield put(redeemInviteSuccess())
     navigate(Screens.VerificationEducationScreen)
     // Note: We are ok with this succeeding or failing silently in the background,
     // user will have another chance to register DEK when sending their first tx
     yield spawn(registerAccountDek, result.newAccount)
-  } else if (result.success === false) {
+  } else if (result?.success === false) {
     Logger.debug(TAG, 'Redeem Invite failed')
     yield put(redeemInviteFailure())
+  } else if (cancel) {
+    ValoraAnalytics.track(OnboardingEvents.invite_redeem_cancel)
+    Logger.debug(TAG, 'Redeem Invite cancelled')
   } else if (timeout) {
     Logger.debug(TAG, 'Redeem Invite timed out')
     ValoraAnalytics.track(OnboardingEvents.invite_redeem_timeout)
@@ -313,16 +330,16 @@ export function* redeemInviteSaga({ inviteCode }: RedeemInviteAction) {
   }
 }
 
-export function* doRedeemInvite(inviteCode: string) {
+export function* doRedeemInvite(tempAccountPrivateKey: string) {
   try {
     ValoraAnalytics.track(OnboardingEvents.invite_redeem_start)
-    const tempAccount = privateKeyToAddress(inviteCode)
+    const tempAccount = privateKeyToAddress(tempAccountPrivateKey)
     Logger.debug(TAG + '@doRedeemInvite', 'Invite code contains temp account', tempAccount)
 
     const [tempAccountBalanceWei, newAccount]: [BigNumber, string] = yield all([
       call(fetchTokenBalanceInWeiWithRetry, CURRENCY_ENUM.DOLLAR, tempAccount),
       call(getOrCreateAccount),
-      call(addTempAccountToWallet, inviteCode),
+      call(addTempAccountToWallet, tempAccountPrivateKey),
     ])
 
     if (tempAccountBalanceWei.isLessThanOrEqualTo(0)) {
@@ -377,17 +394,17 @@ export function* skipInvite() {
   }
 }
 
-function* addTempAccountToWallet(inviteCode: string) {
+function* addTempAccountToWallet(tempAccountPrivateKey: string) {
   Logger.debug(TAG + '@addTempAccountToWallet', 'Attempting to add temp wallet')
   try {
     // Import account into the local geth node
     const wallet: UnlockableWallet = yield call(getWallet)
-    const account = privateKeyToAddress(inviteCode)
+    const account = privateKeyToAddress(tempAccountPrivateKey)
     const password: string = yield call(getPasswordSaga, account, false, true)
-    const tempAccount = yield call([wallet, wallet.addAccount], inviteCode, password)
+    const tempAccount = yield call([wallet, wallet.addAccount], tempAccountPrivateKey, password)
     Logger.debug(TAG + '@addTempAccountToWallet', 'Account added', tempAccount)
   } catch (e) {
-    if (e.toString().includes('account already exists')) {
+    if (e.message === ErrorMessages.GETH_ACCOUNT_ALREADY_EXISTS) {
       Logger.warn(TAG + '@addTempAccountToWallet', 'Account already exists, using it')
       return
     }
@@ -442,7 +459,14 @@ export function* moveAllFundsFromAccount(
 
   const context = newTransactionContext(TAG, 'Transfer from temp wallet')
   // Temporarily hardcoding gas estimate to save time on estimation
-  yield call(sendTransaction, tx.txo, account, context, SEND_TOKEN_GAS_ESTIMATE)
+  yield call(
+    sendTransaction,
+    tx.txo,
+    account,
+    context,
+    SEND_TOKEN_GAS_ESTIMATE,
+    AccountActions.CANCEL_CREATE_OR_RESTORE_ACCOUNT
+  )
   Logger.debug(TAG + '@moveAllFundsFromAccount', 'Done withdrawal')
 }
 

packages/mobile/src/escrow/saga.ts
@@ -130,7 +130,7 @@ function* withdrawFromEscrow() {
     ])
     const account: string = yield call(getConnectedUnlockedAccount)
     const tmpWalletPrivateKey: string | null = yield select(
-      (state: RootState) => state.invite.redeemedInviteCode
+      (state: RootState) => state.invite.redeemedTempAccountPrivateKey
     )
 
     if (!tmpWalletPrivateKey || !isValidPrivateKey(tmpWalletPrivateKey)) {

packages/mobile/src/invite/EnterInviteCode.tsx
@@ -10,13 +10,16 @@ import { ActivityIndicator, StyleSheet, Text, View } from 'react-native'
 import { SafeAreaInsetsContext } from 'react-native-safe-area-context'
 import { connect } from 'react-redux'
 import { OnboardingEvents } from 'src/analytics/Events'
-import { AnalyticsPropertiesList } from 'src/analytics/Properties'
 import CodeInput, { CodeInputStatus } from 'src/components/CodeInput'
 import DevSkipButton from 'src/components/DevSkipButton'
 import { CELO_FAUCET_LINK, SHOW_GET_INVITE_LINK } from 'src/config'
 import i18n, { Namespaces, withTranslation } from 'src/i18n'
 import { redeemInvite, skipInvite } from 'src/invite/actions'
-import { extractValidInviteCode, getValidInviteCodeFromReferrerData } from 'src/invite/utils'
+import {
+  ExtractedInviteCodeAndPrivateKey,
+  extractInviteCodeAndPrivateKey,
+  extractValuesFromDeepLink,
+} from 'src/invite/utils'
 import { HeaderTitleWithSubtitle, nuxNavigationOptions } from 'src/navigator/Headers.v2'
 import { navigate } from 'src/navigator/NavigationService'
 import { Screens } from 'src/navigator/Screens'
@@ -89,10 +92,11 @@ export class EnterInviteCode extends React.Component<Props, State> {
 
   checkForInviteCode = async () => {
     // Check deeplink
-    const validCode = await getValidInviteCodeFromReferrerData()
-    if (validCode) {
-      this.setState({ inputValue: validCode })
-      this.props.redeemInvite(validCode)
+    const extractedValues: ExtractedInviteCodeAndPrivateKey = await extractValuesFromDeepLink()
+    if (extractedValues) {
+      const { inviteCode, privateKey } = extractedValues
+      this.setState({ inputValue: inviteCode })
+      this.props.redeemInvite(privateKey)
       return
     }
   }
@@ -101,15 +105,20 @@ export class EnterInviteCode extends React.Component<Props, State> {
     navigateToURI(CELO_FAUCET_LINK)
   }
 
-  onPressSkip = () => {
+  skipInvite = () => {
     this.props.skipInvite()
   }
 
+  navigateToVerification = () => {
+    navigate(Screens.VerificationEducationScreen)
+  }
+
   onInputChange = (value: string) => {
-    const inviteCode = extractValidInviteCode(value)
-    if (inviteCode) {
+    const extractedValues: ExtractedInviteCodeAndPrivateKey = extractInviteCodeAndPrivateKey(value)
+    if (extractedValues) {
+      const { inviteCode, privateKey } = extractedValues
       this.setState({ inputValue: inviteCode })
-      this.props.redeemInvite(inviteCode)
+      this.props.redeemInvite(privateKey)
     } else {
       this.setState({ inputValue: value })
     }
@@ -120,12 +129,51 @@ export class EnterInviteCode extends React.Component<Props, State> {
   }
 
   shouldShowClipboard = (clipboardContent: string): boolean => {
-    const inviteCode = extractValidInviteCode(clipboardContent)
-    return !!inviteCode && !this.state.inputValue.toLowerCase().startsWith(inviteCode.toLowerCase())
+    const extractedValues: ExtractedInviteCodeAndPrivateKey = extractInviteCodeAndPrivateKey(
+      clipboardContent
+    )
+    return (
+      !!extractedValues &&
+      !this.state.inputValue.toLowerCase().startsWith(extractedValues.inviteCode.toLowerCase())
+    )
+  }
+
+  renderFooterButton = () => {
+    const { t, isRedeemingInvite, redeemComplete } = this.props
+
+    if (SHOW_GET_INVITE_LINK) {
+      return (
+        <Text style={styles.askInviteText}>
+          <Trans i18nKey="inviteCode.nodeCodeInviteLink" ns={Namespaces.onboarding}>
+            <Text onPress={this.onPressGoToFaucet} style={styles.askInviteLink} />
+            <Text onPress={this.skipInvite} style={styles.askInviteLink} />
+          </Trans>
+        </Text>
+      )
+    }
+
+    // This only displays in edge cases where auto-navigation after redemption is unsuccessful
+    if (redeemComplete) {
+      return (
+        <TextButton style={styles.bottomButton} onPress={this.navigateToVerification}>
+          {t('global:done')}
+        </TextButton>
+      )
+    }
+
+    if (!isRedeemingInvite) {
+      return (
+        <TextButton style={styles.bottomButton} onPress={this.skipInvite}>
+          {t('inviteCode.noCode')}
+        </TextButton>
+      )
+    }
+
+    return null
   }
 
   render() {
-    const { t, isRedeemingInvite, isSkippingInvite, redeemComplete, account } = this.props
+    const { t, isRedeemingInvite, isSkippingInvite, redeemComplete } = this.props
     const { keyboardVisible, inputValue } = this.state
 
     let codeStatus = CodeInputStatus.INPUTTING
@@ -134,13 +182,6 @@ export class EnterInviteCode extends React.Component<Props, State> {
     } else if (redeemComplete) {
       codeStatus = CodeInputStatus.ACCEPTED
     }
-    const backAnalyticsEvents: [keyof AnalyticsPropertiesList] = [
-      OnboardingEvents.create_account_cancel,
-    ]
-
-    if (isRedeemingInvite) {
-      backAnalyticsEvents.push(OnboardingEvents.invite_redeem_cancel)
-    }
 
     return (
       <HeaderHeightContext.Consumer>
@@ -148,7 +189,9 @@ export class EnterInviteCode extends React.Component<Props, State> {
           <SafeAreaInsetsContext.Consumer>
             {(insets) => (
               <View style={styles.container}>
-                <UseBackToWelcomeScreen backAnalyticsEvents={backAnalyticsEvents} />
+                <UseBackToWelcomeScreen
+                  backAnalyticsEvents={[OnboardingEvents.create_account_cancel]}
+                />
                 <DevSkipButton nextScreen={Screens.VerificationEducationScreen} />
                 <KeyboardAwareScrollView
                   style={headerHeight ? { marginTop: headerHeight } : undefined}
@@ -167,6 +210,7 @@ export class EnterInviteCode extends React.Component<Props, State> {
                       inputPlaceholder={t('inviteCode.codePlaceholder')}
                       onInputChange={this.onInputChange}
                       shouldShowClipboard={this.shouldShowClipboard}
+                      testID={'inviteCodeInput'}
                     />
                     {isRedeemingInvite && (
                       <View style={styles.loadingContainer}>
@@ -180,24 +224,7 @@ export class EnterInviteCode extends React.Component<Props, State> {
                       <ActivityIndicator size="large" color={colors.greenBrand} />
                     </View>
                   )}
-                  <View>
-                    {SHOW_GET_INVITE_LINK ? (
-                      <Text style={styles.askInviteText}>
-                        <Trans i18nKey="inviteCode.nodeCodeInviteLink" ns={Namespaces.onboarding}>
-                          <Text onPress={this.onPressGoToFaucet} style={styles.askInviteLink} />
-                          <Text onPress={this.onPressSkip} style={styles.askInviteLink} />
-                        </Trans>
-                      </Text>
-                    ) : !isRedeemingInvite ? (
-                      <TextButton
-                        style={styles.bottomButton}
-                        onPress={this.onPressSkip}
-                        disabled={isRedeemingInvite || !!account}
-                      >
-                        {t('inviteCode.noCode')}
-                      </TextButton>
-                    ) : null}
-                  </View>
+                  <View>{this.renderFooterButton()}</View>
                 </KeyboardAwareScrollView>
                 <KeyboardSpacer onToggle={this.onToggleKeyboard} />
               </View>

packages/mobile/src/invite/__snapshots__/EnterInviteCode.test.tsx.snap
@@ -165,6 +165,7 @@ exports[`EnterInviteCode Screen renders correctly 1`] = `
                         undefined,
                       ]
                     }
+                    testID="inviteCodeInput"
                     underlineColorAndroid="transparent"
                     value=""
                   />

packages/mobile/src/invite/actions.ts
@@ -82,12 +82,12 @@ export const sendInviteFailure = (error: ErrorMessages): SendInviteFailureAction
 
 export interface RedeemInviteAction {
   type: Actions.REDEEM_INVITE
-  inviteCode: string
+  tempAccountPrivateKey: string
 }
 
-export const redeemInvite = (inviteCode: string): RedeemInviteAction => ({
+export const redeemInvite = (tempAccountPrivateKey: string): RedeemInviteAction => ({
   type: Actions.REDEEM_INVITE,
-  inviteCode,
+  tempAccountPrivateKey,
 })
 
 export interface RedeemInviteSuccessAction {

packages/mobile/src/invite/reducer.ts
@@ -1,3 +1,4 @@
+import { Actions as AccountActions, ActionTypes as AccountActionTypes } from 'src/account/actions'
 import { Actions, ActionTypes, InviteDetails } from 'src/invite/actions'
 import { getRehydratePayload, REHYDRATE, RehydrateAction } from 'src/redux/persist-helper'
 import { RootState } from 'src/redux/reducers'
@@ -8,7 +9,7 @@ export interface State {
   isSkippingInvite: boolean
   invitees: InviteDetails[]
   redeemComplete: boolean
-  redeemedInviteCode: string | null
+  redeemedTempAccountPrivateKey: string | null
 }
 
 export const initialState: State = {
@@ -17,12 +18,12 @@ export const initialState: State = {
   isSkippingInvite: false,
   invitees: [],
   redeemComplete: false,
-  redeemedInviteCode: null,
+  redeemedTempAccountPrivateKey: null,
 }
 
 export const inviteReducer = (
   state: State | undefined = initialState,
-  action: ActionTypes | RehydrateAction
+  action: ActionTypes | AccountActionTypes | RehydrateAction
 ): State => {
   switch (action.type) {
     case REHYDRATE: {
@@ -54,7 +55,7 @@ export const inviteReducer = (
     case Actions.REDEEM_INVITE:
       return {
         ...state,
-        redeemedInviteCode: action.inviteCode,
+        redeemedTempAccountPrivateKey: action.tempAccountPrivateKey,
         isRedeemingInvite: true,
       }
     case Actions.REDEEM_INVITE_SUCCESS:
@@ -64,6 +65,12 @@ export const inviteReducer = (
         isRedeemingInvite: false,
       }
     case Actions.REDEEM_INVITE_FAILURE:
+      return {
+        ...state,
+        redeemComplete: false,
+        isRedeemingInvite: false,
+      }
+    case AccountActions.CANCEL_CREATE_OR_RESTORE_ACCOUNT:
       return {
         ...state,
         redeemComplete: false,

packages/mobile/src/invite/utils.test.ts
@@ -1,55 +1,66 @@
 import dynamicLinks from '@react-native-firebase/dynamic-links'
 import {
   createInviteCode,
-  extractValidInviteCode,
-  getValidInviteCodeFromReferrerData,
+  extractInviteCodeAndPrivateKey,
+  extractValuesFromDeepLink,
   isValidPrivateKey,
 } from 'src/invite/utils'
 
-describe(extractValidInviteCode, () => {
-  it('extracts invite code correctly', () => {
-    expect(extractValidInviteCode('nothing')).toBeNull()
-    expect(
-      extractValidInviteCode(
-        'Something something pFCr5NAAf/vUcWypJiQFnF6DHI+6vCGxMhhShki07ow= another thing else'
-      )
-    ).toBe('0xa450abe4d0007ffbd4716ca92624059c5e831c8fbabc21b13218528648b4ee8c')
-    expect(
-      extractValidInviteCode(
-        'Something something pFCr5NAAf/vUcWypJiQFnF6DHI+6vCGxMhhShki07ow=another thing else'
-      )
-    ).toBe('0xa450abe4d0007ffbd4716ca92624059c5e831c8fbabc21b13218528648b4ee8c')
+describe(extractInviteCodeAndPrivateKey, () => {
+  it('parses invite code and extracts private key correctly', () => {
+    let extractedValues = extractInviteCodeAndPrivateKey('nothing')
+    expect(extractedValues).toBeNull()
+
+    extractedValues = extractInviteCodeAndPrivateKey(
+      'Something something pFCr5NAAf/vUcWypJiQFnF6DHI+6vCGxMhhShki07ow= another thing else'
+    )
+    expect(extractedValues?.privateKey).toBe(
+      '0xa450abe4d0007ffbd4716ca92624059c5e831c8fbabc21b13218528648b4ee8c'
+    )
+    expect(extractedValues?.inviteCode).toBe('pFCr5NAAf/vUcWypJiQFnF6DHI+6vCGxMhhShki07ow=')
+
     // Old SMS format
-    expect(
-      extractValidInviteCode(
-        'Hi Lorem Ipsum! I would like to invite you to join the Celo payments network.' +
-          ' Your invite code is: pFCr5NAAf/vUcWypJiQFnF6DHI+6vCGxMhhShki07ow='
-      )
-    ).toBe('0xa450abe4d0007ffbd4716ca92624059c5e831c8fbabc21b13218528648b4ee8c')
+    extractedValues = extractInviteCodeAndPrivateKey(
+      'Hi Lorem Ipsum! I would like to invite you to join the Celo payments network.' +
+        ' Your invite code is: pFCr5NAAf/vUcWypJiQFnF6DHI+6vCGxMhhShki07ow='
+    )
+    expect(extractedValues?.privateKey).toBe(
+      '0xa450abe4d0007ffbd4716ca92624059c5e831c8fbabc21b13218528648b4ee8c'
+    )
+    expect(extractedValues?.inviteCode).toBe('pFCr5NAAf/vUcWypJiQFnF6DHI+6vCGxMhhShki07ow=')
+
     // New SMS format
-    expect(
-      extractValidInviteCode(
-        'Hi Ashish! I would like to invite you to join the Celo payments network.' +
-          ' Your invite code is: ndoILWBXFR1+C59M3QKcEA7rWP7+2u5XQKC1gTemXBo=. You can install the Celo ' +
-          'application from the following link: https://celo.page.link/ogoJowDDP5wi1BZN7'
-      )
-    ).toBe('0x9dda082d6057151d7e0b9f4cdd029c100eeb58fefedaee5740a0b58137a65c1a')
+    extractedValues = extractInviteCodeAndPrivateKey(
+      'Hi Ashish! I would like to invite you to join the Celo payments network.' +
+        ' Your invite code is: ndoILWBXFR1+C59M3QKcEA7rWP7+2u5XQKC1gTemXBo=. You can install the Celo ' +
+        'application from the following link: https://celo.page.link/ogoJowDDP5wi1BZN7'
+    )
+    expect(extractedValues?.privateKey).toBe(
+      '0x9dda082d6057151d7e0b9f4cdd029c100eeb58fefedaee5740a0b58137a65c1a'
+    )
+    expect(extractedValues?.inviteCode).toBe('ndoILWBXFR1+C59M3QKcEA7rWP7+2u5XQKC1gTemXBo=')
+
     // Source of https://github.com/celo-org/celo-monorepo/issues/1487 ?
-    expect(
-      extractValidInviteCode(
-        'Hi Izzy! I would like to invite you to join the Celo payments network.' +
-          ' Your invite code is: ESnrL7zNxmP0kjpklcNbCWJJgStYn3xM0dugHh7a9yQ=. ' +
-          'You can install the Celo application from the following link: https://celo.page.link/XYyu9Mi7sz4YNsiM7'
-      )
-    ).toBe('0x1129eb2fbccdc663f4923a6495c35b096249812b589f7c4cd1dba01e1edaf724')
+    extractedValues = extractInviteCodeAndPrivateKey(
+      'Hi Izzy! I would like to invite you to join the Celo payments network.' +
+        ' Your invite code is: ESnrL7zNxmP0kjpklcNbCWJJgStYn3xM0dugHh7a9yQ=. ' +
+        'You can install the Celo application from the following link: https://celo.page.link/XYyu9Mi7sz4YNsiM7'
+    )
+    expect(extractedValues?.privateKey).toBe(
+      '0x1129eb2fbccdc663f4923a6495c35b096249812b589f7c4cd1dba01e1edaf724'
+    )
+    expect(extractedValues?.inviteCode).toBe('ESnrL7zNxmP0kjpklcNbCWJJgStYn3xM0dugHh7a9yQ=')
+
     // And now in es_419
-    expect(
-      extractValidInviteCode(
-        'Hola Izzy! Me gustaría invitarte a que te unas a la red de pagos de Celo. ' +
-          'Tu código de invitación es: ESnrL7zNxmP0kjpklcNbCWJJgStYn3xM0dugHh7a9yQ=. ' +
-          'Podés instalarte la aplicación Celo desde el siguiente vínculo: https://celo.page.link/XYyu9Mi7sz4YNsiM7'
-      )
-    ).toBe('0x1129eb2fbccdc663f4923a6495c35b096249812b589f7c4cd1dba01e1edaf724')
+    extractedValues = extractInviteCodeAndPrivateKey(
+      'Hola Izzy! Me gustaría invitarte a que te unas a la red de pagos de Celo. ' +
+        'Tu código de invitación es: ESnrL7zNxmP0kjpklcNbCWJJgStYn3xM0dugHh7a9yQ=. ' +
+        'Podés instalarte la aplicación Celo desde el siguiente vínculo: https://celo.page.link/XYyu9Mi7sz4YNsiM7'
+    )
+    expect(extractedValues?.privateKey).toBe(
+      '0x1129eb2fbccdc663f4923a6495c35b096249812b589f7c4cd1dba01e1edaf724'
+    )
+    expect(extractedValues?.inviteCode).toBe('ESnrL7zNxmP0kjpklcNbCWJJgStYn3xM0dugHh7a9yQ=')
   })
 })
 
@@ -81,43 +92,56 @@ describe(isValidPrivateKey, () => {
   })
 })
 
-describe(getValidInviteCodeFromReferrerData, () => {
+describe(extractValuesFromDeepLink, () => {
   const host = 'http://example.com'
   const getInitialLink = dynamicLinks().getInitialLink as jest.Mock
   it('returns null with invalid data', async () => {
     getInitialLink.mockResolvedValueOnce({ url: host })
-    expect(await getValidInviteCodeFromReferrerData()).toBeNull()
+    expect(await extractValuesFromDeepLink()).toBeNull()
     getInitialLink.mockResolvedValueOnce({ url: `${host}?x=a` })
-    expect(await getValidInviteCodeFromReferrerData()).toBeNull()
+    expect(await extractValuesFromDeepLink()).toBeNull()
   })
 
   it('gets a valid code from referrer data', async () => {
     getInitialLink.mockResolvedValueOnce({
       url: `${host}?invite-code=0xa450abe4d0007ffbd4716ca92624059c5e831c8fbabc21b13218528648b4ee8c`,
     })
-    expect(await getValidInviteCodeFromReferrerData()).toBe(
+
+    let extractedValues = await extractValuesFromDeepLink()
+    expect(extractedValues?.privateKey).toBe(
+      '0xa450abe4d0007ffbd4716ca92624059c5e831c8fbabc21b13218528648b4ee8c'
+    )
+    expect(extractedValues?.inviteCode).toBe(
       '0xa450abe4d0007ffbd4716ca92624059c5e831c8fbabc21b13218528648b4ee8c'
     )
 
     getInitialLink.mockResolvedValueOnce({
       url: `${host}?invite-code%3D0xa450abe4d0007ffbd4716ca92624059c5e831c8fbabc21b13218528648b4ee8c`,
     })
-    expect(await getValidInviteCodeFromReferrerData()).toBe(
+    extractedValues = await extractValuesFromDeepLink()
+    expect(extractedValues?.privateKey).toBe(
+      '0xa450abe4d0007ffbd4716ca92624059c5e831c8fbabc21b13218528648b4ee8c'
+    )
+    expect(extractedValues?.inviteCode).toBe(
       '0xa450abe4d0007ffbd4716ca92624059c5e831c8fbabc21b13218528648b4ee8c'
     )
 
     getInitialLink.mockResolvedValueOnce({
       url: `${host}?invite-code%3D0ZdSckCiUkiUy5cQMuEv7DucMR%2BEewMx7fmyDd3rm4U%3D`,
     })
-    expect(await getValidInviteCodeFromReferrerData()).toBe(
+    extractedValues = await extractValuesFromDeepLink()
+    expect(extractedValues?.privateKey).toBe(
       '0xd197527240a2524894cb971032e12fec3b9c311f847b0331edf9b20dddeb9b85'
     )
+    expect(extractedValues?.inviteCode).toBe('0ZdSckCiUkiUy5cQMuEv7DucMR+EewMx7fmyDd3rm4U=')
 
     getInitialLink.mockResolvedValueOnce({
       url: `${host}?invite-code=p9f1XCB7kRAgIbLvHhiGvx2Ps9HlWMkyEF9ywkj9xT8=`,
     })
-    expect(await getValidInviteCodeFromReferrerData()).toBe(
+    extractedValues = await extractValuesFromDeepLink()
+    expect(extractedValues?.privateKey).toBe(
       '0xa7d7f55c207b91102021b2ef1e1886bf1d8fb3d1e558c932105f72c248fdc53f'
     )
+    expect(extractedValues?.inviteCode).toBe('p9f1XCB7kRAgIbLvHhiGvx2Ps9HlWMkyEF9ywkj9xT8=')
   })
 })

TuCopFinance/TuCopWallet

commit 00c33e08a5fe3b861ecbe5ab4aebdec9f50de2cc

[Wallet] Fix user seeing the enter invite code screen after upgrading to 1.1.0 (#5000)
### Description

There was a regression introduced by the change in https://github.com/celo-org/celo-monorepo/pull/4977/files#diff-55bba9805f8b51290d1be787ca2cbca6L507-R506

See details in #4997

Though it might looks confusing, `redeemComplete` needs to be set to true when skipping the invite code to avoid this problem.
This is similar to what is done when restoring an account from a seed phrase in https://github.com/celo-org/celo-monorepo/blob/910ab0a5218c05f712422b774dcff1aedf495289/packages/mobile/src/import/saga.ts#L79

Additional note: In the long term I think we would benefit from using more meaningful actions which update multiple parts of the state tree in an atomic manner.
Currently there are parts of the code which dispatch multiple actions sequentially, i.e. using actions as "glorified setters"
See description of the problem here: https://twitter.com/dan_abramov/status/800310164792414208?s=20

### Other changes

None.

### Tested

Added new test.

### Related issues

- Fixes #4997

### Backwards compatibility

Yes.

packages/mobile/src/invite/saga.ts
@@ -45,6 +45,8 @@ import {
   sendInviteFailure,
   sendInviteSuccess,
   SENTINEL_INVITE_COMMENT,
+  skipInviteFailure,
+  skipInviteSuccess,
   storeInviteeData,
 } from 'src/invite/actions'
 import { createInviteCode } from 'src/invite/utils'
@@ -382,15 +384,20 @@ export function* skipInvite() {
   try {
     ValoraAnalytics.track(OnboardingEvents.invite_redeem_skip_start)
     yield call(getOrCreateAccount)
+    // TODO: refactor this, the multiple dispatch calls are somewhat confusing
+    // (`setHasSeenVerificationNux` though the user hasn't seen it),
+    // we should prefer a more atomic approach with a meaningful action type
     yield put(refreshAllBalances())
     yield put(setHasSeenVerificationNux(true))
     Logger.debug(TAG + '@skipInvite', 'Done skipping invite')
     ValoraAnalytics.track(OnboardingEvents.invite_redeem_skip_complete)
     navigateHome()
+    yield put(skipInviteSuccess())
   } catch (e) {
     Logger.error(TAG, 'Failed to skip invite', e)
     ValoraAnalytics.track(OnboardingEvents.invite_redeem_skip_error, { error: e.message })
     yield put(showError(ErrorMessages.ACCOUNT_SETUP_FAILED))
+    yield put(skipInviteFailure())
   }
 }
 

packages/mobile/src/invite/saga.test.ts
@@ -10,7 +10,8 @@ import { PincodeType } from 'src/account/reducer'
 import { showError } from 'src/alert/actions'
 import { ErrorMessages } from 'src/app/ErrorMessages'
 import { generateShortInviteLink } from 'src/firebase/dynamicLinks'
-import { updateE164PhoneNumberAddresses } from 'src/identity/actions'
+import { refreshAllBalances } from 'src/home/actions'
+import { setHasSeenVerificationNux, updateE164PhoneNumberAddresses } from 'src/identity/actions'
 import {
   InviteBy,
   redeemInvite,
@@ -18,14 +19,18 @@ import {
   redeemInviteSuccess,
   sendInvite,
   SENTINEL_INVITE_COMMENT,
+  skipInvite as skipInviteAction,
+  skipInviteSuccess,
   storeInviteeData,
 } from 'src/invite/actions'
 import {
   generateInviteLink,
   moveAllFundsFromAccount,
+  skipInvite,
   watchRedeemInvite,
   watchSendInvite,
 } from 'src/invite/saga'
+import { navigateHome } from 'src/navigator/NavigationService'
 import { getSendFee } from 'src/send/saga'
 import { fetchDollarBalance, transferStableToken } from 'src/stableToken/actions'
 import { transactionConfirmed } from 'src/transactions/actions'
@@ -265,5 +270,24 @@ describe(generateInviteLink, () => {
       appStoreId: '1482389446',
       bundleId: 'org.celo.mobile.alfajores',
     })
+  })
+})
+
+describe(skipInvite, () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('updates the state and navigates to the home screen', async () => {
+    await expectSaga(skipInvite)
+      .provide([[call(getOrCreateAccount), mockAccount]])
+      .withState(state)
+      .put(skipInviteSuccess())
+      .put(refreshAllBalances())
+      .put(setHasSeenVerificationNux(true))
+      .dispatch(skipInviteAction())
+      .run()
+
+    expect(navigateHome).toHaveBeenCalledWith()
   })
 })